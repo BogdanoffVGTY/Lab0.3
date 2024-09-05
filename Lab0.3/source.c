c695c531dd011ca4e49c8ee7e0fcec9cf3355c62
Admin <Admin@DESKTOP-NHEHS5K>
2024-09-05 12:57:27 +0300

    Добавьте файлы проекта.

---
+++ Lab0.3/source.c
@@ -0,0 +1,13 @@
+#include <locale.h>
+#include <stdio.h>
+main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts(" -                           ");
+	puts("-  - __                      ");
+	puts("    |  |__                ");
+	puts("   _|__|__|_________        ");
+	puts("  |                /         ");
+	puts("  |_______________/          ");
+	return 5;
+}
